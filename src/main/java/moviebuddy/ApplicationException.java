16b33b519360a6d4594163adb898dc59f76438c7
EunjoungAhn <aej9300@gmail.com>
2022-06-26 19:01:07 +0900

    First commit gradle project

---
+++ src/main/java/moviebuddy/ApplicationException.java
@@ -0,0 +1,50 @@
+package moviebuddy;
+
+/**
+ * @author springrunner.kr@gmail.com
+ * RuntimeException 상속받아 발생하는 오류를 처리하기 위한 ul 클래스 입니다. 
+ */
+@SuppressWarnings("serial")
+public class ApplicationException extends RuntimeException {
+
+    public ApplicationException(String message) {
+        super(message);
+    }
+
+    public ApplicationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public static class CommandNotFoundException extends ApplicationException {
+
+        public CommandNotFoundException() {
+            super("command not found.");
+        }
+
+    }
+
+    public static class UndefinedCommandActionException extends ApplicationException {
+
+        public UndefinedCommandActionException() {
+            super("command action is undefined.");
+        }
+
+    }
+
+    public static class InvalidCommandArgumentsException extends ApplicationException {
+
+        public InvalidCommandArgumentsException() {
+            super("input error, please try again!");
+        }
+
+        public InvalidCommandArgumentsException(String message) {
+            super(message);
+        }
+
+        public InvalidCommandArgumentsException(Throwable cause) {
+            super(String.format("%s: %s", cause.getClass().getSimpleName(), cause.getMessage()), cause);
+        }
+
+    }
+
+}
