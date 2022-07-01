6c2b381377a5e48eb2d580da05a58be340073e8d
EunjoungAhn <aej9300@gmail.com>
2022-07-01 15:29:54 +0900

    section03_2 gradle test에서 Junit test로 변경처리

--- src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
+++ src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
@@ -2,14 +2,19 @@ package moviebuddy.domain;
 
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class JaxbMovieReaderTest {
 	
-	public static void main(String[] args) {
+//	JUnit 기반 테스트 코드로 변경
+	@Test
+	void NotEmpty_LoadedMovies() {
 		JaxbMovieReader movieReader = new JaxbMovieReader();
 		
 		List<Movie> movies = movieReader.loadMovies();
 		//movies.size() =>> XML 문서에 등록된 영화 수와 동일한가?
-		MovieFinerTest.assertEquals(1375, movies.size());
+		Assertions.assertEquals(1375, movies.size());
 		
 	}
 	
